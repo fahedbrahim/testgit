6a97269aae6906c58fb25b413678702dab0170f1
yazidam <ahmedyazid.mejri@esprit.tn>
2021-03-14 19:49:34 +0100

    pi push

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,8 @@ router.get('/ahmed', function (req, res, next) {
   res.send('this is ahmed');
 });
 
+router.get('/pi', function (req, res, next) {
+  res.send('this is pi');
+});
+
 module.exports = router;
